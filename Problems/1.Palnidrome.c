e316ce4db67dd1cd0dfff350714f02d2edc578e7
Yashwanth S <83501889+nameisyashwanth@users.noreply.github.com>
2021-05-23 13:58:47 +0530

    Update and rename 26)palnidrome or not.c to 1.Palnidrome.c

--- Problems/1.Palnidrome.c
+++ Problems/1.Palnidrome.c
@@ -1,3 +1,7 @@
+/*Program to find whether the given number is a palindrome or not
+Palnidrome:A palindrome is a word, number, phrase, or other sequence of characters which reads the same backward as forward
+Ex:1221, 12521*/
+
 #include<stdio.h>
 int main()
 {
