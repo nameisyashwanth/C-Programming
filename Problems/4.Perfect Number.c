48a5e1a67e27e6b439f37eaa2658993357035c36
Yashwanth S <83501889+nameisyashwanth@users.noreply.github.com>
2021-05-23 14:35:52 +0530

    Update and rename 29)perfect number.c to 4.Perfect Number.c

--- Problems/4.Perfect Number.c
+++ Problems/4.Perfect Number.c
@@ -1,3 +1,7 @@
+/*Program to find Perfect number
+Prefect number: A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself
+Ex: 28 = 1  + 2 + 4 + 7 + 11 (sum of divisors)*/
+
 #include<stdio.h>
 #include<conio.h>
 int main()
