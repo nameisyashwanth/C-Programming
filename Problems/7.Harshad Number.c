e6d49f4cf7650f2deacf4facd3a2cdc7da87e86d
Yashwanth S <83501889+nameisyashwanth@users.noreply.github.com>
2021-05-23 14:46:20 +0530

    Update and rename 32)harshad number.c to 7.Harshad Number.c

--- Problems/7.Harshad Number.c
+++ Problems/7.Harshad Number.c
@@ -1,3 +1,9 @@
+/*Program to find Harshad Number
+Harshad number: A harshad number (or Niven number) in a given number base is an integer that is divisible by the sum of its digits when written in that base. 
+Ex: 21/(2+!) = 7
+    81/(8+1) = 9
+    153/(1+5+3) = 7  */
+
 #include<stdio.h>
 #include<math.h>
 int main()
@@ -17,3 +23,4 @@ int main()
     else
         printf("The number %d is not Harshad number",n);
 }
+:
